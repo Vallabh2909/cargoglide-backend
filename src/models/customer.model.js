1cd92052dfccaff23beeb700723f5fd8c76d93ea
Vallabh2909 <vallabhwasule913@gmail.com>
2024-11-24 12:42:24 +0530

    first commit

---
+++ src/models/customer.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const customerSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    iecCode: {
+      type: String,
+      required: true,
+      match: [/^[A-Z]{3}\d{7}$/, 'IEC code should be 3 letters followed by 7 digits'],
+    },
+    doc: {
+      type: String,
+      required: true
+    },
+  },
+  { timestamps: true }
+);
+
+const Customer = mongoose.model('Customer', customerSchema);
+
+export default Customer;
